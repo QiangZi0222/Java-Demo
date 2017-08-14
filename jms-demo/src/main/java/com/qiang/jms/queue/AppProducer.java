bfd987a3590b14dd5d4ed1ffcca41541c2b7780c
QiangZi <fushiqiang0222@163.com>
2017-08-14 22:08:48 +0800

    Update AppProducer.java

--- jms-demo/src/main/java/com/qiang/jms/queue/AppProducer.java
+++ jms-demo/src/main/java/com/qiang/jms/queue/AppProducer.java
@@ -14,7 +14,7 @@ import javax.jms.*;
 public class AppProducer {
 
     // 61616端口是Activemq默认的端口
-    private static final String url = "tcp://192.168.0.107:61616";
+    private static final String url = "failover:(tcp://192.168.0.107:61617,tcp://192.168.0.107:61618)?randomize=true";
 
     private static final String queueName = "queue-demo";
 
